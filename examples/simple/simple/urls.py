643debdb98d0f7c8486885e24bd91e981e574516
Jon Robison <narfman0@gmail.com>
2017-03-04 22:52:20 -0500

    -Add examples section, with simple app configured to use cow

---
+++ examples/simple/simple/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from cow import urls as cow_urls
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include(cow_urls)),
+]
